66ef9acb52cdd9af5b34c75b11395a4a3771738d
SKruthoff <s.kruthoff@2degrees-investing.org>
2023-10-20 11:00:19 +0200

    onboard tiltProfiles

---
+++ tiltProfiles.py
@@ -0,0 +1,17 @@
+from functions.processing import generate_table
+
+generate_table('ictr_company_result_raw')
+
+generate_table('ictr_product_result_raw')
+
+generate_table('istr_company_result_raw')
+
+generate_table('istr_product_result_raw')
+
+generate_table('pctr_company_result_raw')
+
+generate_table('pctr_product_result_raw')
+
+generate_table('pstr_company_result_raw')
+
+generate_table('pstr_product_result_raw')
