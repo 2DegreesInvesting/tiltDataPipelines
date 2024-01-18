5d869a184d38ed94b450fcef101ab27ecb77e7f0
SvenKruthoff <s.kruthoff@2degrees-investing.org>
2024-01-18 14:17:39 +0100

    remove leftover files

--- Matching.py
+++
@@ -1,4 +0,0 @@
-from functions.processing import generate_table
-
-generate_table("product_matching_complete_all_cases_raw")
-generate_table("labelled_activity_v1.0_raw")
--- tiltIndicatorAfter.py
+++
@@ -1,6 +0,0 @@
-from functions.processing import generate_table
-
-generate_table("mapper_ep_ei_raw")
-generate_table("ep_companies_raw")
-generate_table("ei_input_data_raw")
-generate_table("ei_activities_overview_raw")
