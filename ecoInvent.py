4d09e9e796cbcb2107c8cecaea4f65b10ee8a5f3
SvenKruthoff <s.kruthoff@2degrees-investing.org>
2024-01-23 11:28:42 +0100

    format pipeline files

--- ecoInvent.py
+++ ecoInvent.py
@@ -19,19 +19,21 @@ Note:
 """
 from functions.processing import generate_table
 
-generate_table('geographies_raw')
-# generate_table('geographies_transform')
+    if __name__ == '__main__':
 
-generate_table('undefined_ao_raw')
-generate_table('cut_off_ao_raw')
-generate_table('en15804_ao_raw')
-generate_table('consequential_ao_raw')
-# generate_table('products_activities_transformed')
+    generate_table('geographies_raw')
+    # generate_table('geographies_transform')
 
-generate_table('lcia_methods_raw')
+    generate_table('undefined_ao_raw')
+    generate_table('cut_off_ao_raw')
+    generate_table('en15804_ao_raw')
+    generate_table('consequential_ao_raw')
+    # generate_table('products_activities_transformed')
 
-generate_table('impact_categories_raw')
+    generate_table('lcia_methods_raw')
 
-generate_table('intermediate_exchanges_raw')
+    generate_table('impact_categories_raw')
 
-generate_table('elementary_exchanges_raw')
+    generate_table('intermediate_exchanges_raw')
+
+    generate_table('elementary_exchanges_raw')
