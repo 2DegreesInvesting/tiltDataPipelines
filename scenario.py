a3c063bebd427b424d58c294220c32236416b20b
SvenKruthoff <s.kruthoff@2degrees-investing.org>
2024-07-05 17:58:53 +0200

    test commit

--- scenario.py
+++ scenario.py
@@ -3,12 +3,14 @@ from functions.processing_datamodel import generate_table as generate_table_data
 
 if __name__ == '__main__':
 
-# raw layer
-    
+    # raw layer
+
     generate_table_raw('scenario_targets_IPR_raw')
     generate_table_raw('scenario_targets_WEO_raw')
 
 # data model layer
-    
+
     generate_table_datamodel('scenario_targets_IPR_datamodel')
     generate_table_datamodel('scenario_targets_WEO_datamodel')
+
+    # test
