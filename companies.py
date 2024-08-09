e3855c67bc2423de7e1b5def4c84b1ba7d896fea
Alex Jang <aljang@deloitte.nl>
2024-08-09 14:28:16 +0200

    revert

--- companies.py
+++ companies.py
@@ -4,22 +4,22 @@ from functions.processing_datamodel import generate_table as generate_table_data
 
 if __name__ == "__main__":
     # raw layer
-    # generate_table_raw("tiltledger_embedding_raw")
+    generate_table_raw("tiltledger_embedding_raw")
 
-    # generate_table_raw("companies_embedding_raw")
-    # generate_table_raw('companies_companyinfo_raw')
-    # generate_table_raw('country_raw')
+    generate_table_raw("companies_embedding_raw")
+    generate_table_raw("companies_companyinfo_raw")
+    generate_table_raw("country_raw")
 
-    #     # company_info SBI activities
-    # generate_table_datamodel('SBI_activities_raw')
+    # company_info SBI activities
+    generate_table_datamodel("SBI_activities_raw")
 
-    # # datamodel layer
+    # datamodel layer
     generate_table_datamodel("companies_datamodel")
 
-    # generate_table_datamodel('companies_match_result_datamodel')
+    generate_table_datamodel("companies_match_result_datamodel")
 
-    #     # company_info SBI activities
-    # generate_table_datamodel('SBI_activities_datamodel')
-    # generate_table_datamodel('companies_SBI_activities_datamodel')
+    # company_info SBI activities
+    generate_table_datamodel("SBI_activities_datamodel")
+    generate_table_datamodel("companies_SBI_activities_datamodel")
 
     # Europages products
