c5b03583b0dde8aae69e07e851eb640a65745642
Sohail el Sabrouty <selsabrouty@deloitte.nl>
2024-09-25 17:09:12 +0200

    Scope 1 onboarded
    the table structure only considers relevant columns (can still change depending on what the output needs to look like)

--- indicators.py
+++ indicators.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
 
 #   enriched_datamodel layer
     # generate_table_enriched_datamodel("emission_profile_ledger_enriched")
-    generate_table_enriched_datamodel("emission_profile_ledger_upstream_enriched")
+    # generate_table_enriched_datamodel("emission_profile_ledger_upstream_enriched")
     # generate_table_enriched_datamodel("sector_profile_ledger_enriched")
     # generate_table_enriched_datamodel("sector_profile_ledger_upstream_enriched")
     # generate_table_enriched_datamodel("transition_risk_ledger_enriched")
+    # generate_table_enriched_datamodel("scope_1_indicator_enriched")
 
